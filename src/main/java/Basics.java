a4b85fbd3fa7052c009794419ff872f8622bd2b1
chrischin1 <christian.bongalon@mail.utoronto.ca>
2022-10-09 20:09:04 -0400

    Completed exercise 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,7 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
-
+        int my_variable = 100;
 
 
         /* Do not remove the line below: if you did task 2 correctly, then
@@ -99,10 +99,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
-
-
-    }
-
+        for (int i = 10; i >= 0; i -= 1)
+            System.out.println("Current count: " + i);
+        }
     /**
      *  6. Below is a method named split which returns a String,
      *    as indicated by the 'String' part of the header. It takes one
@@ -141,9 +140,9 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
-
-        // Fill in the rest of the body here
-
+        for (String s : to_split.split(" ")){
+            ret.append(s.charAt(0));
+        }
         return ret.toString();
     }
 
@@ -163,14 +162,11 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
-
-        /* TODO (Task 5): Complete this method body using a for-loop.
-         *                You can find the length of an array by using the
-         *                .length attribute (e.g. arr.length)
-         *                You can index into arrays as we do in Python
-         *                (e.g. arr[i] gives you the item at index i).
-         */
-
+        for (int i = 0; i < arr.length; i++){
+            if (i % 2 == 1){
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
